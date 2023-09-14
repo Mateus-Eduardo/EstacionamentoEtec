a9257b982ea6334531d39cfe5e22f2e2df72ee0d
Mateus Eduardo Silvério <115047768+Mateus-Eduardo@users.noreply.github.com>
2023-09-13 22:23:29 -0300

    criando o teste estacionamento

---
+++ TesteEstacionamentoEtec/PatioTeste.cs
@@ -0,0 +1,29 @@
+﻿using Etec.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TesteEstacionamentoEtec
+{
+    public class PatioTeste
+    {
+        [Fact]
+        public void TestePesquisarVeiculo()
+        {
+            // Arrange
+            Patio estacionamento = new Patio();
+            Veiculo veiculo = new Veiculo();
+            var veiculos = new List<Veiculo> { veiculo };
+            var pesquisaVeiculo = new Veiculo { Placa = "XYZ123", Modelo = "Carro" };
+
+            // Act
+            var resultado = estacionamento.PesquisaVeiculo(pesquisaVeiculo.Placa);
+
+            // Assert
+            Assert.Equal(veiculo, resultado);
+        }
+    }
+}
